032593b3abb6d10025640107f86039a6d13c14dc
SOYOUZ69 <omarmsaad4@gmail.com>
2023-12-01 15:03:27 +0100

    get interet experience phone

---
+++ user/get_phone_experience_interet_u.php
@@ -0,0 +1,25 @@
+<?php 
+include '../connection.php';
+
+$id = 1;
+
+
+$sql = "SELECT * FROM `user` WHERE `id`='$id'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $phone = $row["phone"];
+    $phoneEncoded = json_decode($phone, JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+    
+    $experience = $row["experience"];
+    $experienceEncoded = json_decode($experience, JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+    
+    $interet = $row["interet"];
+    $interetEncoded = json_decode($interet, JSON_UNESCAPED_UNICODE | JSON_HEX_AMP);
+    
+    echo json_encode(array("phone" => $phoneEncoded,"experience" => $experienceEncoded,"interet" => $interetEncoded));
+} else {
+    echo json_encode(array("get" => false));
+}
+?>
