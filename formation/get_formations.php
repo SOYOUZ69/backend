d48090c26e54be45f1d12d63b5a2c3e980258787
SOYOUZ69 <omarmsaad4@gmail.com>
2023-12-21 03:38:36 +0100

    get with index and limit

--- formation/get_formations.php
+++ formation/get_formations.php
@@ -1,13 +1,19 @@
 <?php
 include '../connection.php';
 
-$sql = "SELECT * FROM `formation`";
+if (isset($_POST['lastId'])){
+    $lastId = $_POST['lastId'];
+    $sql = "SELECT * FROM `formation` where id > $lastId LIMIT 10";
+
 $result = $conn->query($sql);
 
 $formations = [];
 
+
 while ($row = $result->fetch_assoc()) {
     $formations[] = $row;
+    $lastId = $row['id'];
 }
 
-echo json_encode(array("formations" => $formations));
+echo json_encode(array("formations" => $formations, "lastId" => $lastId));}
+else{echo json_encode(array("formations" =>false));}
