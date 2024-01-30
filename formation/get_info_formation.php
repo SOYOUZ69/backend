297eb8265e6a53cdc2aba6771a1f31801af88c80
SOYOUZ69 <omarmsaad4@gmail.com>
2024-01-30 23:12:19 +0100

    user formation info

--- formation/get_info_formation.php
+++ formation/get_info_formation.php
@@ -10,18 +10,24 @@ $data = json_decode($rawBody, true);
 if (isset($data['id'])) {
     $Id = (int) $data['id'];  // Ensure lastId is an integer
 
-    $sql = "SELECT * FROM `formation` WHERE id = $Id LIMIT 10";
+    $sql = "SELECT * FROM `formation` WHERE id = $Id ";
 
     $result = $conn->query($sql);
     $formations = $result->fetch_assoc();
+    $use=$formations['id_poster'];
+    $sql = "SELECT username,picture FROM `user` WHERE id = $use";
+
+    $result = $conn->query($sql);
+    $user = $result->fetch_assoc();
  
 
    
 
-    echo json_encode(array("formations" => $formations));
+    echo json_encode(array("formations" => $formations,"user" => $user));
 } else {
     echo json_encode(array("formations" => false));
 }
+
 
 
 ?>
