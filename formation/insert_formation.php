611af6b4fc3b1f88a9fdd44fb530204445d93169
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-02 19:32:17 +0100

    change row insert formation

--- formation/insert_formation.php
+++ formation/insert_formation.php
@@ -1,25 +1,29 @@
 <?php
 include '../connection.php';
-if($_POST["nom"]){
-$nom = $_POST["nom"];
-$description = $_POST["description"];
+$rawBody = file_get_contents('php://input');
 
-$link = $_POST["link"];
-$trailer = $_POST["trailer"];
-$price = $_POST["price"];
-$date = $_POST["date"];
-$company_url = $_POST["company_url"];
-$metakey = $_POST["metakey"];
-$id_poster = $_POST["id_poster"];
+// Decode JSON data (assuming the ID is sent as JSON)
+$data = json_decode($rawBody, true);
+if($data["nom"]){
+$nom = $data["nom"];
+$description = $data["description"];
+
+$link = $data["link"];
+
+$price = $data["price"];
+$date = $data["date"];
+$company_url = $data["company_url"];
+$metakey = $data["metakey"];
+$id_poster = $data["id_poster"];
 
 $logo = "";
-if (!empty($_FILES['logo']['name'])) {
+if (!empty($_FILES['cover']['name'])) {
     $errors = array();
-    $file_name = mysqli_real_escape_string($conn, $_FILES['logo']['name']);
-    $file_size = $_FILES['logo']['size'];
-    $file_tmp = $_FILES['logo']['tmp_name'];
-    $file_type = $_FILES['logo']['type'];
-    $exp = explode('.', $_FILES['logo']['name']);
+    $file_name = mysqli_real_escape_string($conn, $_FILES['cover']['name']);
+    $file_size = $_FILES['cover']['size'];
+    $file_tmp = $_FILES['cover']['tmp_name'];
+    $file_type = $_FILES['cover']['type'];
+    $exp = explode('.', $_FILES['cover']['name']);
     $end_expl = end($exp);
     $file_ext = strtolower($end_expl);
 
@@ -41,7 +45,7 @@ if (!empty($_FILES['logo']['name'])) {
     }
 }
 
-$sql="INSERT INTO `formation`( `nom`, `description`, `cover`, `link`, `trailer`, `price`, `date`, `company_url`, `metakey`, `id_poster`) VALUES ( $nom, $description, $logo, $link, $trailer, $price, $date, $company_url, $metakey, $id_poster)";
+$sql="INSERT INTO `formation`( `nom`, `description`, `cover`, `link`, `price`, `date`, `company_url`, `metakey`, `id_poster`) VALUES ( $nom, $description, $logo, $link, $price, $date, $company_url, $metakey, $id_poster)";
 $result=$conn->query($sql);
 if($result){
   $insertedID = mysqli_insert_id($conn);
