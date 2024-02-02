7320d50d5b29647bebb1ee1c7cfbb41feda40d51
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-03 00:09:09 +0100

    big change

--- formation/insert_formation.php
+++ formation/insert_formation.php
@@ -1,56 +1,61 @@
 <?php
 include '../connection.php';
-$rawBody = file_get_contents('php://input');
 
-// Decode JSON data (assuming the ID is sent as JSON)
-$data = json_decode($rawBody, true);
-if($data["nom"]){
-$nom = $data["nom"];
-$description = $data["description"];
+if ($_POST["nom"]) {
+    $nom = $_POST["nom"];
+    $description = $_POST["description"];
+    $link = $_POST["link"];
+    $price = $_POST["price"];
+    $date = $_POST["date"];
+    $company_url = $_POST["company_url"];
+    $metakey = $_POST["metakey"];
+    $id_poster = $_POST["id_poster"];
 
-$link = $data["link"];
+    $logo = "";
+    if (!empty($_FILES['cover']['name'])) {
+        $errors = array();
+        $file_name = mysqli_real_escape_string($conn, $_FILES['cover']['name']);
+        $file_size = $_FILES['cover']['size'];
+        $file_tmp = $_FILES['cover']['tmp_name'];
+        $file_type = $_FILES['cover']['type'];
+        $exp = explode('.', $_FILES['cover']['name']);
+        $end_expl = end($exp);
+        $file_ext = strtolower($end_expl);
 
-$price = $data["price"];
-$date = $data["date"];
-$company_url = $data["company_url"];
-$metakey = $data["metakey"];
-$id_poster = $data["id_poster"];
+        $expensions = array("jpeg", "jpg", "png");
 
-$logo = "";
-if (!empty($_FILES['cover']['name'])) {
-    $errors = array();
-    $file_name = mysqli_real_escape_string($conn, $_FILES['cover']['name']);
-    $file_size = $_FILES['cover']['size'];
-    $file_tmp = $_FILES['cover']['tmp_name'];
-    $file_type = $_FILES['cover']['type'];
-    $exp = explode('.', $_FILES['cover']['name']);
-    $end_expl = end($exp);
-    $file_ext = strtolower($end_expl);
+        if (in_array($file_ext, $expensions) === false) {
+            $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        }
 
-    $expensions = array("jpeg", "jpg", "png");
+        $logo = time() . '_' . $file_name;
 
-    if (in_array($file_ext, $expensions) === false) {
-        $errors[] = "extension not allowed, please choose a JPEG or PNG file.";
+        if (empty($errors)) {
+            move_uploaded_file($file_tmp, "../uploads/formation/" . $logo);
+        } else {
+            print_r($errors);
+        }
     }
 
-
-
-    $logo = time() . '_' . $file_name;
-
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "../uploads/formations/" . $logo);
-        //echo "Success";
+    $sql = "INSERT INTO `formation`(`nom`, `description`, `cover`, `link`, `price`, `date`, `company_url`, `metakey`, `id_poster`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    
+    $stmt = $conn->prepare($sql);
+    
+    if ($stmt) {
+        $stmt->bind_param("ssssdsssi", $nom, $description, $logo, $link, $price, $date, $company_url, $metakey, $id_poster);
+        
+        $result = $stmt->execute();
+        
+        if ($result) {
+            $insertedID = mysqli_insert_id($conn);
+            echo json_encode(array("success" => true, "id" => $insertedID));
+        } else {
+            echo json_encode(array("success" => false));
+        }
+        
+        $stmt->close();
     } else {
-        print_r($errors);
+        echo json_encode(array("success" => false, "error" => $conn->error));
     }
 }
-
-$sql="INSERT INTO `formation`( `nom`, `description`, `cover`, `link`, `price`, `date`, `company_url`, `metakey`, `id_poster`) VALUES ( $nom, $description, $logo, $link, $price, $date, $company_url, $metakey, $id_poster)";
-$result=$conn->query($sql);
-if($result){
-  $insertedID = mysqli_insert_id($conn);
-  echo json_encode(array("success" => true, "id" => $insertedID));
-
-}else{echo json_encode(array("success" => false));
-}}
 ?>
