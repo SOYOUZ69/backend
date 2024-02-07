7fc0f875504cf3a0ff8aaf40ffbd94888734150b
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-07 02:28:48 +0100

    events anwar

---
+++ events/get_events.php
@@ -0,0 +1,27 @@
+<?php
+include '../connection.php';
+
+// Access raw request body
+$rawBody = file_get_contents('php://input');
+
+// Decode JSON data (assuming the ID is sent as JSON)
+$data = json_decode($rawBody, true);
+
+if (isset($data['ev'])) {
+    // Ensure lastId is an integer
+$date_n=date("Y-m-d H:i:s");
+$sql = "SELECT * FROM `formation` WHERE `date` >= '" . $date_n . "'";
+
+$result = $conn->query($sql);
+
+$formations = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $formations[] = $row;
+       
+    }
+
+    echo json_encode(array("formations" => $formations));
+} else {
+    echo json_encode(array("formations" => false));
+}
