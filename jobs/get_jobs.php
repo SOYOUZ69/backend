d24360c0b7a5f6afa5756f09d7f904a21b6d1297
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-16 20:41:05 +0100

    saves jobs

--- jobs/get_jobs.php
+++ jobs/get_jobs.php
@@ -7,12 +7,14 @@ $rawBody = file_get_contents('php://input');
 // Decode JSON data (assuming the ID is sent as JSON)
 $data = json_decode($rawBody, true);
 
-if (isset($data['last_id'])) {
+if (isset($data['last_id'])&&isset($data['id_user'])) {
     $lastId = (int) $data['last_id'];  // Ensure lastId is an integer
-
-    $sql = "SELECT job_post.*, user.username,user.picture
+    $id_user =(int) $data['id_user'];
+    $sql = "SELECT job_post.*, user.username, user.picture,
+    (CASE WHEN favoris.id IS NOT NULL THEN true ELSE false END) AS save
 FROM job_post
 JOIN user ON job_post.id_poster = user.id
+LEFT JOIN favoris ON favoris.object_type = 'job' AND favoris.id_object = job_post.id AND favoris.id_u = $id_user
 WHERE job_post.id > $lastId
 LIMIT 10;
 ";
