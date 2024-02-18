255723269206bbbdc5a5d599e5517e375255a4f2
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-18 03:41:22 +0100

    Update blog_details.php

--- blogs/blog_details.php
+++ blogs/blog_details.php
@@ -7,35 +7,44 @@ $rawBody = file_get_contents('php://input');
 // Decode JSON data (assuming the ID is sent as JSON)
 $data = json_decode($rawBody, true);
 
-if (isset($data['id'])) {
-    $Id = (int) $data['id'];  // Ensure lastId is an integer
+if (isset($data['id']) && isset($data['id_u'])) {
+    $Id = (int) $data['id'];
+    $id_u = $data['id_u']; // Ensure lastId is an integer
 
-    
-    $sql = "SELECT comment.id, comment.object_type, comment.id_object, comment.id_u, comment.message, comment.date, user.username, user.picture, COUNT(upvotes.id) AS upvote_count
+    $sql = "
+    SELECT 
+        comment.id, 
+        comment.object_type, 
+        comment.id_object, 
+        comment.id_u, 
+        comment.message, 
+        comment.date, 
+        user.username, 
+        user.picture, 
+        COUNT(upvotes.id) AS upvote_count,
+        CASE WHEN comment.id_u = ? THEN true ELSE false END AS comentu,
+        CASE WHEN upvotes.id_u = ? THEN true ELSE false END AS like_u
     FROM comment
     JOIN user ON comment.id_u = user.id
     LEFT JOIN upvotes ON comment.id_object = upvotes.id_o AND upvotes.type = 'comment'
-    WHERE comment.id_object = $Id
+    WHERE comment.id_object = ?
     GROUP BY comment.id, comment.object_type, comment.id_object, comment.id_u, comment.message, comment.date, user.username, user.picture;
     ";
 
-    $result = $conn->query($sql);
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sss', $id_u, $id_u, $Id); // sss represents three string parameters, adjust if needed
+    $stmt->execute();
+
+    $result = $stmt->get_result();
 
     $comments = [];
 
     while ($row = $result->fetch_assoc()) {
         $comments[] = $row;
-        $lastId = $row['id'];
     }
- 
-
-   
 
     echo json_encode(array("comments" => $comments));
 } else {
     echo json_encode(array("comments" => false));
 }
-
-
-
 ?>
