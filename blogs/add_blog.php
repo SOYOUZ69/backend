fee1fc1891c38204d65e2e4c24eb8262c8976e2a
SOYOUZ69 <omarmsaad4@gmail.com>
2024-02-27 22:25:44 +0100

    Update add_blog.php

--- blogs/add_blog.php
+++ blogs/add_blog.php
@@ -4,18 +4,19 @@ $rawBody = file_get_contents('php://input');
 
 // Decode JSON data (assuming the ID is sent as JSON)
 $data = json_decode($rawBody, true);
-if (isset($data["id_poster"], $data["full_desc"], $data["metakey"])) {
+if (isset($data["id_poster"], $data["full_desc"],$data["bief_desc"], $data["metakey"])) {
     $id_poster = $data["id_poster"];
     $full_desc = $data["full_desc"];
+    $bief_desc = $data["bief_desc"];
     $metakey = $data["metakey"];
 
-    $sql = "INSERT INTO `blog_post`(`full_desc`, `metakey`, `id_poster`, `state`,`date`) VALUES (?, ?, ?, 0,now())";
+    $sql = "INSERT INTO `blog_post`(`full_desc`,`bief_desc`, `metakey`, `id_poster`, `state`,`date`) VALUES (?, ?, ?, ?, 0,now())";
 
     $stmt = $conn->prepare($sql);
 
     if ($stmt) {
         // Assurez-vous que les types dans bind_param correspondent aux types réels dans la base de données
-        $stmt->bind_param("ssi", $full_desc, $metakey, $id_poster);
+        $stmt->bind_param("sssi", $full_desc,$bief_desc, $metakey, $id_poster);
 
         $result = $stmt->execute();
 
